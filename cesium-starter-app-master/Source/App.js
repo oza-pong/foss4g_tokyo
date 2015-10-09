5ccc82656c1ce0edd271a4eaa935e7ba520d9a2e
oza-pong <risei.heian.kashiba@gmail.com>
2015-10-09 11:21:18 +0900

    h

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -10,3 +10,13 @@ point.add({
             color : Cesium.Color.YELLOW
     }
 });
+
+var point2 = viewer.entities;
+
+point2.add({
+        position : Cesium.Cartesian3.fromDegrees(-65.59777, 40.03883),
+        point : {
+            pixelSize : 10,
+            color : Cesium.Color.BLUE
+    }
+});
