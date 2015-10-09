4ebc749c86fea1044a234737672f6b06e29c218f
oza-pong <risei.heian.kashiba@gmail.com>
2015-10-09 11:23:36 +0900

    ji

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -15,8 +15,21 @@ var point2 = viewer.entities;
 
 point2.add({
         position : Cesium.Cartesian3.fromDegrees(-65.59777, 40.03883),
-        point : {
+        point2 : {
             pixelSize : 10,
             color : Cesium.Color.BLUE
+    }
+});
+
+//ラインを追加
+var line1 = viewer.entities;
+
+line1.add({
+    name : 'Red line on the surface',
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
+                                                        -125, 35]),
+        width : 5,
+        material : Cesium.Color.RED
     }
 });
